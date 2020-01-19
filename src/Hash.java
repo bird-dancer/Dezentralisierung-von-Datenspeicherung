f427305b7a8bf5e2bcf5e86f46301b27c44a0c1b
Felix Dumbeck <53702818+f-eliks@users.noreply.github.com>
2020-01-19 14:53:15 +0100

    Add files via upload

---
+++ src/Hash.java
@@ -0,0 +1,45 @@
+package src;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Hash{
+    /**
+     * 
+     * @param bytes
+     * @return
+     * @throws NoSuchAlgorithmException
+     */
+    public static String hash(byte [] bytes) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        bytes = md.digest(bytes);
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes)    sb.append(String.format("%02x", b));
+        return sb.toString();
+    }
+
+    /**
+     * hashes a String using SHA3<br>
+     * inspired by: <a href="https://www.baeldung.com/sha-256-hashing-java#message-digest">https://www.baeldung.com/sha-256-hashing-java#message-digest</a>
+     * @param originalString
+     * @return hashed String in a hexadecimal-format
+     * @throws NoSuchAlgorithmException
+     * 
+     */
+    public String hash(String originalString) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        return bytesToHex(md.digest(originalString.getBytes(StandardCharsets.UTF_8)));
+    }
+    
+    /**
+     * 
+     * @param bytes
+     * @return a number with base 16 as a String
+     */
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes)    sb.append(String.format("%02x", b));
+        return sb.toString();
+    }
+}
